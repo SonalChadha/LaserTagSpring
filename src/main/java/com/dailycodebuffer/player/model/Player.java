49a7ace4f3de8e9287eb4dc96de81b9ef6f11147
Sonal Chadha <sonalchadha25@gmail.com>
2022-03-05 22:49:04 -0600

    Add files via upload

--- src/main/java/com/dailycodebuffer/player/model/Player.java
+++ src/main/java/com/dailycodebuffer/player/model/Player.java
@@ -1,1 +1,15 @@
+package com.dailycodebuffer.player.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+}
