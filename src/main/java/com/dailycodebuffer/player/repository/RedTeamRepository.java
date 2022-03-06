ff90e0e4b6a2bf7765f7280c67be4cb8f87ff209
Sonal Chadha <sonalchadha25@gmail.com>
2022-03-05 22:50:06 -0600

    Add files via upload

--- src/main/java/com/dailycodebuffer/player/repository/RedTeamRepository.java
+++ src/main/java/com/dailycodebuffer/player/repository/RedTeamRepository.java
@@ -1,10 +1,10 @@
 package com.dailycodebuffer.player.repository;
 
-import com.dailycodebuffer.player.entity.GreenPlayerEntity;
+import com.dailycodebuffer.player.entity.RedPlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GreenTeamRepository extends JpaRepository<GreenPlayerEntity, Long> {
+public interface RedTeamRepository extends JpaRepository<RedPlayerEntity, Long> {
 
 }
