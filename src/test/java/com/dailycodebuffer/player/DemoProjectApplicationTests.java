76a85f75418046fcafe647f0411f8fb3155b4101
Sonal Chadha <sonalchadha25@gmail.com>
2022-03-05 22:53:07 -0600

    Add files via upload

--- src/test/java/com/dailycodebuffer/player/DemoProjectApplicationTests.java
+++ src/test/java/com/dailycodebuffer/player/DemoProjectApplicationTests.java
@@ -1,1 +1,13 @@
+package com.dailycodebuffer.player;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
